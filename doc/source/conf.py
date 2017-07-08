031afd9d8420cb381f3e9e7e8ad0ac0894b38333
Pedro Cuadra <pjcuadra@gmail.com>
2017-07-08 18:44:21 +0200

    chg: dev: Test setup for downloading images for documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,6 +21,7 @@ print('sys.argv[0] =', sys.argv[0])
 pathname = os.path.dirname(sys.argv[0])
 print('path =', pathname)
 print('full path =', os.path.abspath(pathname))
+print('full path =', os.path.abspath(__file__))
 
 html_theme = "sphinx_rtd_theme"
 
